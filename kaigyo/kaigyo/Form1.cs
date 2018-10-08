93cc2d4f40297c8416be8399f4739dc572d3546d
latefair <39183429+latefair@users.noreply.github.com>
2018-10-08 15:02:21 +0900

    とりあえず形

---
+++ kaigyo/kaigyo/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kaigyo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text_kuso = Clipboard.GetText();
+            string text_nice = text_kuso.Replace(Environment.NewLine, "");
+            string text_verynice = text_nice.Replace(".", "." + Environment.NewLine + Environment.NewLine);
+            Clipboard.SetText(text_verynice);
+        }
+    }
+}
